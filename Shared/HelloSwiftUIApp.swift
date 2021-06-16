b1ad03f68047d676f99fce07881c090ef0883bfc
刘sama <junchen.liu@jiamiantech.com>
2021-06-16 16:31:36 +0800

    Initial Commit

---
+++ Shared/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  Shared
+//
+//  Created by sama 刘 on 2021/6/16.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
